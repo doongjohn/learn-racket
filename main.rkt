5cef5112683b6946a8457d2625df595ffb136fbf
doongjohn <doongjohn@gmail.com>
2023-06-26 16:44:16 +0900

    update

--- main.rkt
+++ main.rkt
@@ -1,19 +1,34 @@
 #lang racket
 
-(require racket/block)
 
+;; function and variable
 (define (hello name)
   (printf "Hello, ~a\n" name))
 
-(define person-name "안유찬")
+(define person-name "John")
+(hello person-name)
+(set! person-name "Steve")
 (hello person-name)
 
+
+;; scoped variable
+(let ()
+  (define hi "hi")
+  (displayln hi))
+
+(local
+  ((define hi "hi"))
+  (displayln hi))
+
+(require racket/block)
 (block
  (define hi "hi")
  (displayln hi))
 
 ;; (displayln hi) ;; <-- error
 
+
+;; if else
 (if #t
     (displayln "true")
     (displayln "false"))
@@ -21,6 +36,8 @@
     (displayln "true")
     (displayln "false"))
 
+
+;; read-line
 (block
  (display ">> ")
  (define input (read-line))
@@ -34,3 +51,8 @@
          (string-prefix? (string-downcase input) "안녕"))
      (displayln "Nice to meet you!")
      (displayln "What??")))
+
+
+;; loop
+(for ([i (list 1 2 3)])
+  (println i))
